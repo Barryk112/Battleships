873c4460eb548b4a06c622d5d3fb63e99e696580
Barryk112 <barrykiely112@gmail.com>
2022-12-28 10:06:34 +0000

    Initial commit, Created main class for battleships board

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+scores = {"player": 0, "computer": 0}
+
+class Board:
+    """
+    Main class for board.
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.guesses = []
+        self.ships = []
+
+
